cc700c8f55890dede5cfef1a3375ed53b62e6fe8
C5OK5Y <claudiokozicky@gmail.com>
2013-08-05 20:31:43 +0200

    cleaned up autoconfig.php

--- misc/autoconfig.php
+++ misc/autoconfig.php
@@ -1,18 +1,13 @@
 <?php
-define("DBNAME", $_SERVER['OPENSHIFT_APP_NAME'] );
-define("DBUSER", $_SERVER['OPENSHIFT_MYSQL_DB_USERNAME'] );
-define("DBPASS", $_SERVER['OPENSHIFT_MYSQL_DB_PASSWORD'] );
-define("DBHOST", $_SERVER['OPENSHIFT_MYSQL_DB_HOST'] . ':' . $_SERVER['OPENSHIFT_MYSQL_DB_PORT'] );
-define("DIRECTORY", $_SERVER['OPENSHIFT_DATA_DIR'] );
-
 $AUTOCONFIG = array(
-  'dbtype'     => 'mysql',
-  'dbname'     => DBNAME,
-  'dbuser'     => DBUSER,
-  'dbpass'     => DBPASS,
-  'dbhost'     => DBHOST,
-  'adminlogin' => 'admin',
-  'adminpass'  => 'password',
-  'directory'  => DIRECTORY,
+    "dbtype"        => "mysql",
+    "dbname"        => getenv('OPENSHIFT_APP_NAME'),
+    "dbuser"        => getenv('OPENSHIFT_MYSQL_DB_USERNAME'),
+    "dbpass"        => getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
+    "dbhost"        => getenv('OPENSHIFT_MYSQL_DB_HOST') . ':' . getenv('OPENSHIFT_MYSQL_DB_PORT'),
+    "dbtableprefix" => "",
+    "adminlogin"    => "admin",
+    "adminpass"     => "password",
+    "directory"     => getenv('OPENSHIFT_DATA_DIR'),
 );
 ?>
