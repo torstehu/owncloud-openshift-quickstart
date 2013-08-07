128eeb0ac6344f910121066220018cd50cae9607
Claudio Kozický <claudiokozicky@gmail.com>
2013-08-07 09:36:56 +0200

    move apps_paths to config.php

--- misc/autoconfig.php
+++ misc/autoconfig.php
@@ -9,32 +9,5 @@ $AUTOCONFIG = array(
     "adminlogin"    => "admin",
     "adminpass"     => "password",
     "directory"     => $_ENV['OPENSHIFT_DATA_DIR'],
-    "apps_paths"    => array(
-        0 => array(
-            'path'     => $_ENV['OPENSHIFT_REPO_DIR'] . '/php/apps',
-            'url'      => '/apps',
-            'writable' => true,
-        ),
-        1 => array(
-            'path'     => $_ENV['OPENSHIFT_REPO_DIR'] . '/extapps/apps',
-            'url'      => '/extapps/apps',
-            'writable' => false,
-        ),
-        2 => array(
-            'path'     => $_ENV['OPENSHIFT_REPO_DIR'] . '/extapps/bookmarks',
-            'url'      => '/extapps/bookmarks',
-            'writable' => false,
-        ),
-        3 => array(
-            'path'     => $_ENV['OPENSHIFT_REPO_DIR'] . '/extapps/calendar',
-            'url'      => '/extapps/calendar',
-            'writable' => false,
-        ),
-        4 => array(
-            'path'     => $_ENV['OPENSHIFT_REPO_DIR'] . '/extapps/contacts',
-            'url'      => '/extapps/contacts',
-            'writable' => false,
-        ),
-    ),
 );
 ?>
